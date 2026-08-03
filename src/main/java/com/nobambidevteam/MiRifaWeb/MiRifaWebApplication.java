d0de14b0c9553cbef693df44401f5f0fa2c7e3bc
Martin Alejandro Viscarra <viscarramartin23@gmail.com>
2026-08-03 20:17:49 -0300

    Creación del proyecto
    Creación del proyecto base con su organización inicial de carpetas y dependencias.
    - JDK 21
    - Spring Boot 4.1.0

---
+++ src/main/java/com/nobambidevteam/MiRifaWeb/MiRifaWebApplication.java
@@ -0,0 +1,13 @@
+package com.nobambidevteam.MiRifaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiRifaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiRifaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobambidevteam/MiRifaWeb/MiRifaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobambidevteam.MiRifaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiRifaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
